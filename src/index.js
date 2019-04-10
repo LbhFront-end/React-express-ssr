50ac4d9bbb90ed4c2e4b6c64afbac0790e23b014
Lbh <544289495@qq.com>
2019-04-10 16:29:14 +0800

    基本配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import express from 'express';
+import { renderToString } from 'react-dom/server';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './containers/Home'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const app = express();
+const content = renderToString(<Home />)
+
+app.get('/', (req, res) => {
+  res.send(`
+  <html>
+    <head>
+      <title>ssr</title>
+      <body>
+        ${content}
+      </body>
+    </head>
+  </html>
+  `)
+})
+const server = app.listen(3000);
---
+++ webpack.server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        presets: ['react', 'stage-0', ['env', {
+          // 版本兼容大部分浏览器最后2个
+          targets: {
+            browsers: ['last 2 versions']
+          }
+        }]]
+      }
+    }]
+  }
+}
